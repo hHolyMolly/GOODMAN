e7cde67ebcdc9ca3406a8cc0484d7f666ed0a9cc
HolyMolly <developer.code5417@gmail.com>
2024-05-03 20:14:29 +0300

    Updates

--- js/modules/chunks/tabs.js
+++ js/modules/chunks/tabs.js
@@ -1,13 +1,13 @@
 // Step tabs
 (() => {
-  const tabs = document.querySelectorAll('.step__tabs');
+  const tabs = document.querySelectorAll('.step__tabs, .form-casino-ratings');
 
   tabs.forEach((tab) => {
-    const buttons = tab.querySelectorAll('ul.step__tabs-count li');
+    const buttons = tab.querySelectorAll('ul.step__tabs-count li, .form-casino-ratings__nav li');
 
     buttons.forEach((button, idx) => {
       button.addEventListener('click', () => {
-        const contents = tab.querySelectorAll('.step__tabs-content');
+        const contents = tab.querySelectorAll('.step__tabs-content, .form-casino-ratings-tabs__item');
 
         buttons.forEach((item) => {
           item.classList.remove('_active');
